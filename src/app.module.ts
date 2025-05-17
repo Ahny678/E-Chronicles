77d3adc903facfc2fb1c60293c0f9e327ed8e416
Tiffany Nebo <nebotiffany@gmail.com>
2025-05-17 20:41:58 +0100

    Rate limiting and entry validation done

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,27 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { DiaryModule } from './diary/diary.module';
-
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, DiaryModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    DiaryModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 30, //30 requests per minute
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/diary/dtos/create-diary.dto.ts
+++ src/diary/dtos/create-diary.dto.ts
@@ -1,5 +1,14 @@
+import { IsString, IsNotEmpty } from 'class-validator';
 export class CreateEntryDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   mood: string;
+
+  @IsString()
+  @IsNotEmpty()
   content: string;
 }
