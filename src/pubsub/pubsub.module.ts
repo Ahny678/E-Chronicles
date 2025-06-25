e1658809f40e0b755fd1674dbaa6776f864eca29
Tiffany Nebo <nebotiffany@gmail.com>
2025-06-26 00:46:59 +0100

    feat(subscriptions): Add pubsub resource

---
+++ src/pubsub/pubsub.module.ts
@@ -0,0 +1,44 @@
+// pubsub.module.ts
+import { Module } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+import { PUB_SUB } from './constants/pubsubs.constants';
+
+@Module({
+  providers: [
+    {
+      provide: PUB_SUB,
+      useValue: new PubSub(), // ✅ In-memory for local/dev
+    },
+  ],
+  exports: [PUB_SUB],
+})
+export class PubSubModule {}
+
+/*********************FOR PRODUCTION *******************************/
+
+// import { Module } from '@nestjs/common';
+// import { RedisPubSub } from 'graphql-redis-subscriptions';
+// import Redis from 'ioredis';
+// import { PUB_SUB } from './constants/pubsubs.constants';
+
+// const pubSubProvider = {
+//   provide: 'PUB_SUB',
+//   useFactory: () => {
+//     const options = {
+//       host: 'localhost',
+//       port: 6379,
+//       retryStrategy: (times) => Math.min(times * 50, 2000),
+//     };
+
+//     return new RedisPubSub({
+//       publisher: new Redis(options),
+//       subscriber: new Redis(options),
+//     });
+//   },
+// };
+
+// @Module({
+//   providers: [pubSubProvider],
+//   exports: [PUB_SUB],
+// })
+// export class PubSubModule {}
