d118429780460b7e5409acd2e9490731d3409180
Tiffany Nebo <nebotiffany@gmail.com>
2025-07-01 18:02:28 +0100

    fix: Switch to node 22.16.0 to handle crypto err

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
-import * as crypto from 'crypto';
-if (typeof globalThis.crypto === 'undefined') {
-  (globalThis as any).crypto = crypto;
-}
-// (globalThis as any).crypto = crypto;
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
